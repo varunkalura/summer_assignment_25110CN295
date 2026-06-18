6f8d0ee8f86117454f8857932f0ddb6c63bba022
varunkalura <varunkalura07@gmail.com>
2026-06-18 17:27:55 +0530

    Add files via upload

---
+++ Day_18/q71.cpp
@@ -0,0 +1,32 @@
+//binary search
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int n, int key) {
+    int low = 0, high = n - 1;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] == key) {
+            return mid;  
+        }
+        else if (arr[mid] < key) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    return -1; 
+}
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50, 60, 70};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int key = 50;
+    int result = binarySearch(arr, n, key);
+    if (result != -1)
+        cout << "Element found at index " << result;
+    else
+        cout << "Element not found";
+    return 0;
+}
---
+++ Day_18/q72.cpp
@@ -0,0 +1,19 @@
+//sort array in descending order
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {5, 2, 8, 1, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] < arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
